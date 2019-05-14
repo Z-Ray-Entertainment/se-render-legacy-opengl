bdfc5a0ee5b315980d9143d04c55f1e32ead98d5
Vortex Acherontic <vortex@z-ray.de>
2019-05-14 15:57:30 +0200

    #7 Added Debug Normal renderer

--- SER_GL/src/de/zray/renderbackends/opengl/debug/GLDebugNormal.java
+++ SER_GL/src/de/zray/renderbackends/opengl/debug/GLDebugNormal.java
@@ -8,12 +8,10 @@ package de.zray.renderbackends.opengl.debug;
 import de.zray.se.graphics.semesh.Mesh;
 import de.zray.se.graphics.semesh.MeshData;
 import de.zray.se.graphics.semesh.Normal;
-import de.zray.se.graphics.semesh.Orientation;
 import de.zray.se.graphics.semesh.Vertex;
 import de.zray.se.storages.AssetLibrary;
 import de.zray.se.world.Actor;
 import javax.vecmath.Vector3d;
-import javax.vecmath.Vector3f;
 import static org.lwjgl.opengl.GL11.*;
 
 /**
@@ -22,31 +20,32 @@ import static org.lwjgl.opengl.GL11.*;
  */
 public class GLDebugNormal {
     public void renderNormals(Actor act){
+        Vector3d pos = act.getOrientation().getPositionVec();
+        Vector3d rot = act.getOrientation().getRotationVec();
+        
         for(Mesh mesh : act.getRendableSEMeshes()){
             MeshData mData = AssetLibrary.get().getMesh(mesh.getSEMeshData());
             if(mData.getVertecies().size() == mData.getNormals().size()){
                 long verts = mData.getVertecies().size();
+                glPushMatrix();
+                glTranslated(pos.x, pos.y, pos.z);
+                glRotated(rot.x, 1, 0, 0);
+                glRotated(rot.y, 0, 1, 0);
+                glRotated(rot.z, 0, 0, 1);
+                Vector3d scl = mesh.getOffset().getScaleVec();
+                glScaled(scl.x, scl.y, scl.z);
+                
                 glBegin(GL_LINES);
                 for(int i = 0; i < verts; i++){
                     Vertex v = mData.getVertecies().get(i);
                     Normal n = mData.getNormals().get(i);
-                    Vector3d pos = act.getOrientation().getPositionVec();
-
-                    switch(i%3){
-                        case 0 :
-                            glColor3f(1, 0, 0);
-                            break;
-                        case 1 :
-                            glColor3f(0, 1, 0);
-                            break;
-                        case 2 :
-                            glColor3f(0, 0, 1);
-                            break;
-                    }
-                    glVertex3d(pos.x+v.vX, pos.y+v.vY, pos.z+v.vZ);
-                    glVertex3d(pos.x+v.vX+n.nX, pos.y+v.vY+n.nY, pos.z+v.vZ+n.nZ);
+                    glColor3f(.5f, .5f, 1f);
+                    glVertex3d(v.vX, v.vY, v.vZ);
+                    glVertex3d(v.vX+n.nX, v.vY+n.nY, v.vZ+n.nZ);
                 }
                 glEnd();
+                
+                glPopMatrix();
             }
         }
     }
